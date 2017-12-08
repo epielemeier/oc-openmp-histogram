804c7059098c45f26004c1759e196d90d2347ddc
emilypie <empielemeier@gmail.com>
2017-12-07 22:34:29 -0600

    Compute bin counts via locks instead of critical directives

--- histogram.c
+++ histogram.c
@@ -37,12 +37,10 @@ char* histogram_to_string(struct histogram_type h) {
     return beginning;
 }
 
-void add_datum_to_histogram(float d, struct histogram_type *h) {
-#pragma omp parallel for
+int find_bin_for_datum(float d, struct histogram_type *h) {
     for (int i = 0; i < h->num_bins; i++) {
-        if (d <= h->bin_maxes[i] && (i == 0 || !(d > h->bin_maxes[i - 1]))) {
-#pragma omp critical
-            h->bin_counts[i]++;
+        if (d <= h->bin_maxes[i] && (i == 0 || d > h->bin_maxes[i - 1])) {
+            return i;
         }
     }
 }
@@ -50,19 +48,31 @@ void add_datum_to_histogram(float d, struct histogram_type *h) {
 struct histogram_type compute_histogram(int data_count, float *data, float min_meas, float max_meas, int bin_count) {
     struct histogram_type h = malloc_histogram(bin_count);
     float bin_width = (max_meas - min_meas) / h.num_bins;
-
-    double start_time = omp_get_wtime();
     // compute maxes for each bin
 #pragma omp parallel for
     for (int i = 0; i < h.num_bins; i++)
         h.bin_maxes[i] = min_meas + bin_width * (i + 1);
 
-    // compute counts for each bin
+    // init locks for each bin
+    omp_lock_t *locks = (omp_lock_t *) malloc(sizeof(omp_lock_t*));
 #pragma omp parallel for
+    for (int i = 0; i < h.num_bins; i++)
+        omp_init_lock(&locks[i]);
+
+    // compute counts for each bin
+    int bin;
+#pragma omp parallel for private(bin)
     for (int i = 0; i < data_count; i++) {
-        add_datum_to_histogram(data[i], &h);
+        bin = find_bin_for_datum(data[i], &h);
+        omp_set_lock(&locks[bin]);
+        h.bin_counts[bin]++;
+        omp_unset_lock(&locks[bin]);
     }
-    double end_time = omp_get_wtime();
-    printf("Performance: %f\n", end_time - start_time);
+
+    // free locks
+#pragma omp parallel for
+    for (int i = 0; i < h.num_bins; i++)
+        omp_destroy_lock(&locks[i]);
+
     return h;
 }
--- main.c
+++ main.c
@@ -14,11 +14,14 @@ int main() {
     float data[DATA_COUNT] = {1.3, 2.9, 0.4, 0.3, 1.3, 4.4, 1.7, 0.4, 3.2, 0.3, 4.9, 2.4, 3.1, 4.4, 3.9, 0.4, 4.2, 4.5, 4.9, 0.9};
     float max = find_max(DATA_COUNT, data);
     float min = find_min(DATA_COUNT, data);
-    printf("Max is: %f\tMin is: %f\n", max, min);
 
+    double start_time = omp_get_wtime();
     struct histogram_type h = compute_histogram(DATA_COUNT, data, min, max, bin_count);
+    double end_time = omp_get_wtime();
+
     char *str = histogram_to_string(h);
     printf("Histogram:\n%s\n", str);
+    printf("Performance: %f\n", end_time - start_time);
 
     free(str);
     free_histogram(&h);
